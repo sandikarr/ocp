ee584d59046c23eba10bd79322babfc5b997ef09
ashutoshbhakare <bhakareashutosh@yahoo.com>
2023-01-25 21:44:40 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <h1>My first PHP page</h1>
 
 <?php
-echo "Hello World! FROM OCT BATCH testing webhook learning for KUCL batch"
+echo "Hello World! FROM OCT BATCH testing webhook learning for KUCL batch";
  echo "hello from UNNATI 555555.........";
 ?>
 
