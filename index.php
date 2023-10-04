ec7d6c56fab875a5631cd30b0b1fa8c7cf46d3e2
sandikarr <144679801+sandikarr@users.noreply.github.com>
2023-10-04 17:04:41 +0500

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 <html>
 <body>
 
-<h1>My first PHP page version my Webhooks fromm v3 </h1>
+<h1>My first PHP page version my Webhooks from v5 </h1>
 
 <?php
-echo "hello World from test FROM S2I 2nd time VERSION 3.2 "
+echo "hello World from test FROM S2I 3rd time VERSION 3.2 "
 ?>
 
 </body>
